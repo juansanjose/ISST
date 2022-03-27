084d47a0bb0d9ff520ea3ecda0a8b961a74cce2c
gato manzana <8835298+XSword24@users.noreply.github.com>
2022-03-27 18:48:35 +0200

    Inicio proyecto maven

---
+++ src/test/java/es/upm/dit/isst/ordermanager/OrdermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.ordermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
