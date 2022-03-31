66c1650392f5664a12349666c261fe6f18a79393
gato manzana <8835298+XSword24@users.noreply.github.com>
2022-03-31 19:58:47 +0200

    AreaRepartidor hecha

--- frontend/src/routes/AreaRepartidor.jsx
+++ frontend/src/routes/AreaRepartidor.jsx
@@ -1,56 +1,79 @@
 import * as React from 'react';
-import {Button, Form, Table} from 'react-bootstrap';
-import { useContext } from 'react';
-import {Link } from "react-router-dom";
-
+import {Button, ButtonGroup, Container, Table, Form} from 'react-bootstrap'; 
+import {Link} from "react-router-dom";
+import {useState, useEffect} from 'react';
 
 export default function AreaRepartidor() {
+	const [nombre, setNombre] = useState("");
+	const [pedido, setPedido] = useState([]);
+	const handleSubmit = (e) => {
+        e.preventDefault();
 
-  return (
-    <div>
-		<h2>PEDIDOS ACTUALES</h2>
-		<Table className="bg-light">
-		<thead>
-			<tr>
-				<th>ID del producto</th>
+		alert('You have submitted');
+    }
 
-				<th>Destino</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><Link to="/arearepartidorfinalizar">1</Link></td>
-				<td>Lorem ipsum dolor sit amet, consectetur adipiscing. </td>
-
-			</tr>
-			<tr>
-				<td><Link to="/arearepartidorfinalizar">2</Link></td>
-				<td>Lorem ipsum dolor sit amet, consectetur adipiscing. </td>
-			</tr>
-		</tbody>
-		</Table>
+	useEffect(() => { 
 		
-		<h2>PEDIDOS PENDIENTES</h2>
-		<Table className="bg-light">
-		<thead>
-			<tr>
-				<th>ID del producto</th>
+		// Update the document title using the browser API
+		fetch('/api/pedido/repartidor/'+String(nombre))
+			.then(response => response.json())
+			.then(response => setPedido(response));
+		console.log(pedido);
+	});
 
-				<th>Destino</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><Link to="/arearepartidorconfirmar">3</Link></td>
-				<td>Lorem ipsum dolor sit amet, consectetur adipiscing. </td>
+	const pedidosList = pedido.map(pedido => {
+		return <tr key={pedido.repartidor}>
+                	<td style={{whiteSpace: 'nowrap'}}>{pedido.id}</td>
+                	<td>{pedido.estado}</td>
+                	<td>{pedido.cliente}</td>
+                	<td>{pedido.destino}</td>
+			<td>{pedido.origen}</td>
+                	<td>opinion</td>
+        	</tr>
+    	});
 
-			</tr>
-			<tr>
-				<td><Link to="/arearepartidorconfirmar">4</Link></td>
-				<td>Lorem ipsum dolor sit amet, consectetur adipiscing. </td>
-			</tr>
-		</tbody>
-		</Table>
-	</div>
-  );
+	return (
+     <div>
+		<Container fluid>
+			<h2>HISTORIAL DE PEDIDOS DEL REPARTIDOR: {nombre}</h2>
+		<div>
+            <form className='form' onSubmit={handleSubmit}>
+                <div className='form-control'>
+                    <label htmlFor='firstName'>Name: </label>
+                    <input
+                        type='text'
+                        id='nombre'
+                        name='nombre'
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                    />
+                </div>
+                <button type='submit'>Buscar</button>
+            </form>
+        </div>
+			<Table className="bg-light">
+				<thead>
+					<tr>
+						<th>ID del producto</th>
+
+						<th>Estado del pedido</th>
+
+						<th>Cliente</th>
+
+						<th>Destino</th>
+						
+						<th>Origen</th>
+						
+						<th>Opinión del cliente</th>
+					</tr>
+				</thead>
+			<tbody>
+				{pedidosList}
+			</tbody>
+			</Table>
+		</Container>
+		
+		<h4>Recuerda que solo se pueden evaluar pedidos completados.</h4>
+	 </div>
+	);
 }
