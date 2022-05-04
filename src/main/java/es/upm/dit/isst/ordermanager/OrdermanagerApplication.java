d8a74f9f5c56ad6a3ebc72d20e3595d469acc398
Jose Angel <jose.calderon.villar@alumnos.upm.es>
2022-05-04 16:28:38 +0200

    sistema login hecho

--- src/main/java/es/upm/dit/isst/ordermanager/OrdermanagerApplication.java
+++ src/main/java/es/upm/dit/isst/ordermanager/OrdermanagerApplication.java
@@ -3,6 +3,7 @@ package es.upm.dit.isst.ordermanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OrdermanagerApplication {
 
